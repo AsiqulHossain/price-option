5798ce445e5770f403e16ce4683eea70c2641c41
AsiqulHossain <asiqulhossain@gmail.com>
2025-08-04 00:24:10 +0600

    add description

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <>
         <NavBar />
         <h1>Price Option BD </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse sint cupiditate deserunt perferendis quos ratione labore, amet placeat eos reprehenderit quidem voluptatem tenetur sunt magnam ipsam quaerat possimus modi unde!</p>
     </>
   )
 }
